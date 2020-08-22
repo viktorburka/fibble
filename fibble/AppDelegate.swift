6140a53448de5bce27c75e7726a4f64616a259ad
Viktor Burka <viktorburka.cpp@gmail.com>
2020-08-21 20:31:52 -0600

    Avoid dimming the screen

--- fibble/AppDelegate.swift
+++ fibble/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        application.isIdleTimerDisabled = true
         // Override point for customization after application launch.
         return true
     }
